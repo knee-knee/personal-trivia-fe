c98acfc6dc904c5fd3d9302439bc102029862b1f
Rene Travieso <rene.l.travieso@gmail.com>
2020-04-28 16:33:59 -0400

    adding header

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
+import Header from "./common/Header";
 
 function App() {
-  const route = window.location.pathname; // this is looking at the url
-  if (route === "/about") return <AboutPage />;
-  return <HomePage />;
+  function getPage() {
+    const route = window.location.pathname; // this is looking at the url
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
